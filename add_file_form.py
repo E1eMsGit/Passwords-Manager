8c68149f91a02ee7ad32259d71374ae2bd7ed63b
SAMsoftwareRep <simaginam@gmail.com>
2018-06-07 08:51:19 +0300

    add adding file form

---
+++ add_file_form.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+=======================================================
+Программа для хранения паролей.
+=======================================================
+Модуль функционала формы создания файла.
+=======================================================
+* Ввод имени нового файла паролей.
+=======================================================
+NOTES: Пароль авторизации можно изменить открыв password.bin
+    в любом текстовом редакторе .
+    Сборку файлов form_design.py и icons.py выполнял командами:
+    pyuic5 .\add_file_form.ui -o .\add_file_form_design.py
+    pyrcc5 .\icons.qrc -o  .\icons.py
+    Для конвертации формы нужен файл .ui (создается в qt designer)
+    Для конвертации иконок нужен файл .qrc (создается в текстовом редакторе)
+"""
+import pickle
+
+from PyQt5 import QtWidgets, QtGui
+
+import add_file_form_design
+from icons import *
+
+
+class AddFieDialog(QtWidgets.QDialog):
+    """
+    Главное окно.
+    """
+
+    def __init__(self, main_form):
+        QtWidgets.QDialog.__init__(self)
+
+        self.file_name = None
+        self.main_form = main_form
+
+        self.ui = add_file_form_design.Ui_Form()
+        self.ui.setupUi(self)
+        self.setWindowIcon(QtGui.QIcon(":/images/lock-icon.png"))
+
+        self.search_password_file()
+
+        self.ui.accept_pushButton.clicked.connect(self.add_file)
+
+    def closeEvent(self, e):
+        """
+        Обработка закрытия окна.
+        :param e:
+        :return:
+        """
+        result = QtWidgets.QMessageBox.question(
+            self,
+            self.windowTitle(),
+            "Are you sure you want to exit?",
+            QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+            QtWidgets.QMessageBox.No
+        )
+        if result == QtWidgets.QMessageBox.Yes:
+            e.accept()
+            QtWidgets.QWidget.closeEvent(self, e)
+        else:
+            e.ignore()
+
+    @QtCore.pyqtSlot()
+    def add_file(self):
+        """
+        Создает файл и обновляет список файлов.
+        :return:
+        """
+        self.file_name = self.ui.file_name_lineEdit.text()
+
+        try:
+            with open(self.file_name, "wb") as file:
+                pickle.dump("", file)
+        except FileNotFoundError:
+            QtWidgets.QMessageBox.critical(
+                self,
+                self.windowTitle(),
+                "Enter file name!",
+                QtWidgets.QMessageBox.Ok,
+                QtWidgets.QMessageBox.Ok
+            )
+
+        self.ui.file_name_lineEdit.clear()
+        self.hide()
+
+        self.main_form.listWidget.clear()
+        self.main_form.update_files_list()
+
+
+if __name__ == "__main__":
+    print(__doc__)
